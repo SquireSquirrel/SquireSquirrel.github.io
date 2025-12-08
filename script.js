0d0c84f48defa2f728ec92d9c25fdc476fb56b95
Anders Nyland <52212748+SquireSquirrel@users.noreply.github.com>
2025-12-07 20:42:44 -0500

    Buttons 1

---
+++ script.js
@@ -0,0 +1,9 @@
+function showDiv(divId) {
+    console.log("Button clicked:", id); // test
+    // hide all divs
+    const divs = document.querySelectorAll('.feature_choice');
+    divs.forEach(div => div.style.display = 'none');
+
+    // show the selected div
+    document.getElementById(divId).style.display = 'flex';
+}
