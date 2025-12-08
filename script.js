55a0c4aa4114eb34b0b464b857b86237881b366b
Anders Nyland <52212748+SquireSquirrel@users.noreply.github.com>
2025-12-07 22:20:29 -0500

    Button Fix

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function showDiv(divId) {
-    console.log("Clicking button:", divId);
-    // hide all divs
-    const divs = document.querySelectorAll('.feature_choice');
-    divs.forEach(div => div.style.display = 'none');
+function showDiv(divId, otherIds) {
+    otherIds.forEach(id => {
+        const div = document.getElementById(id);
+        if (div) div.style.display = 'none';
+    });
 
-    // show the selected div
     document.getElementById(divId).style.display = 'flex';
 }
